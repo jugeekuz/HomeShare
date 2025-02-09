19be39b5e109c964e5b72667a6dd08ee1397fce7
Anastasios Diamantis <tdiamantis97@gmail.com>
2025-02-10 00:28:15 +0200

    File Loader Height

--- web-interface/src/components/FileLoader.jsx
+++ web-interface/src/components/FileLoader.jsx
@@ -3,11 +3,10 @@ import { FileContext } from '../contexts/FileContext';
 
 
 const ArrowIcon = () => (
-    <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="45px" height="45px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M8 8L12 4M12 4L16 8M12 4V16M4 20H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
 )
-
 
 const FileLoader = () => {
 
@@ -22,7 +21,7 @@ const FileLoader = () => {
     return (
     <div className="flex items-center justify-center">
         <div 
-            className="flex rounded-full items-center justify-center w-[6rem] h-[6rem] bg-gray-50 shadow-lg cursor-pointer"
+            className="flex rounded-full items-center justify-center w-[5.5rem] h-[5.5rem] bg-blue-50 shadow-lg cursor-pointer"
 
             onClick={() =>  fileInputRef.current?.click()}
         >
