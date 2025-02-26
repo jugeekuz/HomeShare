532bb02232298dfbd7142d81c119c2b43a9d916e
Anastasios Diamantis <tdiamantis97@gmail.com>
2025-02-26 17:32:06 +0200

    Progress bar percentage change

--- web-interface/src/components/ProgressBar.tsx
+++ web-interface/src/components/ProgressBar.tsx
@@ -10,16 +10,25 @@ const ProgressBar = forwardRef<ProgressBarRef, ProgressBarProps>((props, ref) =>
       updateProgress: (newProgress: number) => {
         setProgress(newProgress);
       },
+      getProgress: () => progress,
     }), []);
   
     return (
         <>
         <Progress  
+            size={props.size ? props.size : "md"}
             aria-label="Loading..." 
-            className={props.className} 
+            classNames={{
+                track: "bg-white border border-gray-200",
+                indicator: "bg-blue-500"
+              }}
+            className={props.className}
             color="primary" 
             value={progress}
         />
+        <span className="text-xs text-gray-500 ml-2">
+            {`${progress}%`}
+        </span>
         </>
     );
 });
