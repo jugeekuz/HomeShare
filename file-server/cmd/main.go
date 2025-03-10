61d13ea852bcfb8308b7406e324c4dc96d40a712
Anastasios Diamantis <tdiamantis97@gmail.com>
2025-03-10 22:09:48 +0200

    Add sharing folder cleanup routine

--- file-server/cmd/main.go
+++ file-server/cmd/main.go
@@ -2,13 +2,25 @@
 package main
 
 import (
-	"log"
-	"time"
 	"file-server/internal/app"
+	"file-server/config"
+	"file-server/internal/helpers"
 	"file-server/internal/job"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+	if err := os.MkdirAll(cfg.SharingDir, os.ModePerm); err != nil {
+		log.Fatal("Error while creating sharing folder directory")
+	}
+
+	go func () {
+		_ = helpers.CleanupExpiredFolders(cfg.SharingDir)
+		time.Sleep(30 * time.Minute)
+	}()
 
 	job_timeout := 45 * time.Second
 	jm := job.NewJobManager(job_timeout)
