6799f9041daabcf700c80ef765a5651cd27d0e2b
Anastasios Diamantis <tdiamantis97@gmail.com>
2025-03-11 21:22:18 +0200

    DB Initialization

--- file-server/internal/app/app.go
+++ file-server/internal/app/app.go
@@ -1,35 +1,67 @@
 package app
 
 import (
+	// "context"
 	"file-server/config"
-	"file-server/internal/job"
-	"file-server/internal/uploader"
-	"file-server/internal/sharing"
-	"file-server/internal/downloader"
 	"file-server/internal/auth"
+	"file-server/internal/db"
+	"file-server/internal/downloader"
+	"file-server/internal/job"
+	"file-server/internal/sharing"
+	"file-server/internal/uploader"
+	"fmt"
 	"net/http"
 
 	"github.com/rs/cors"
 )
 
-func SetupServer(jm *job.JobManager) *http.Server {
+// type ContextKey string
+// const UserClaimsKey ContextKey = "userClaims"
+
+// func AuthMiddleware(next http.Handler) http.Handler {
+//     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//         token := r.Header.Get("Authorization")
+//         if token == "" {
+//             http.Error(w, "Unauthorized", http.StatusUnauthorized)
+//             return
+//         }
+// 		// token decode
+//         userClaims := map[string]string{
+//             "folder1": "read",
+//             "folder2": "write",
+//         }
+
+//         ctx := context.WithValue(r.Context(), UserClaimsKey, userClaims)
+
+//         next.ServeHTTP(w, r.WithContext(ctx))
+//     })
+// }
+
+func SetupServer(jm *job.JobManager) (*http.Server, error) {
 	cfg := config.LoadConfig()
+
+	db, err := db.StartConn()
+	if err != nil {
+        return nil, fmt.Errorf("failed to connect to database: %w", err)
+    }
+	user := cfg.User
+	if err := auth.InitializeUserTable(db); err != nil {
+		return nil, err
+	}
+	if _, err := auth.CreateAdminUser(db, user.Username, user.Email, user.Password); err != nil {
+		return nil, err
+	}
+
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"https://kuza.gr", "https://kuza.gr/"},
 		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodOptions},
 	})
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
-		uploader.UploadHandler(w, r, cfg.UploadDir)
-	})
 
-	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
-		downloader.DownloadHandler(w, r, jm)
-	})
-
+	// Unauthenticated endpoints
 	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		auth.LoginHandler(w, r)
+		auth.LoginHandler(w, r, db)
 	})
 
 	mux.HandleFunc("/refresh", func(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +70,15 @@ func SetupServer(jm *job.JobManager) *http.Server {
 
 	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		auth.LogoutHandler(w, r)
+	})
+
+	// Authenticated endpoints
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		uploader.UploadHandler(w, r, cfg.UploadDir)
+	})
+
+	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		downloader.DownloadHandler(w, r, jm)
 	})
 
 	mux.HandleFunc("/share", func(w http.ResponseWriter, r *http.Request) {
@@ -55,5 +96,5 @@ func SetupServer(jm *job.JobManager) *http.Server {
 	return &http.Server{
 		Addr:    ":443",
 		Handler: c.Handler(mux),
-	}
+	}, nil
 }
